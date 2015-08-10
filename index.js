799e8e8d46966665d5ed705d41c99e600e4ad440
pawelrychlik <pawel.rychlik@rspective.pl>
2015-08-10 16:56:37 +0200

    initial filtering - only relevant synsets

--- index.js
+++ index.js
@@ -7,43 +7,47 @@ var word = process.argv[2] || 'rodent';
 
 wordnet.lookup(word, function(synsets) {
 
-    var sorted = _.sortBy(synsets, function(synset) {
-        return 1 - natural.JaroWinklerDistance(word, synset.lemma);
-    });
+    if (!synsets || !synsets.length) {
+        console.log('No synsets found.');
+        return;
+    }
 
-    synset = sorted[0];
-    _.forEach(synsets, function(synset) {
-        console.log('LOOKUP: %j', synset);
+    //-- Take only the most relevant synsets --
+    var relevant = _(synsets)
+        // wrap into object and calculate levenshtein distance
+        .map(function(synset) { return { val: synset, distance: natural.LevenshteinDistance(word, synset.lemma) }; })
+        .sortBy('distance')
+        .filter(function(i) { return i.distance < 4; })
+        // unwrap
+        .pluck('val').value();
 
-        var filteredPointers = synset.ptrs;
-        filteredPointers = synset.ptrs.filter(function(ptr) {
-            return ptr.pointerSymbol === "@";
-        });
+    _.forEach(relevant, function(synset) {
+        // console.log('LOOKUP: %j', synset);
 
-
-        var callback = function(relatedSynsets) {
+        var callback = function(theSynset, relatedSynsets) {
             _.forEach(relatedSynsets, function(relatedSynset) {
-                console.log('%s (%s; %s) ==[%s]==> %s (%s; %s)', synset.lemma, synset.synsetOffset, synset.pos, relatedSynset.pointerSymbol, relatedSynset.lemma, relatedSynset.synsetOffset, relatedSynset.pos);
+                console.log('%s (%s; %s) ==[%s]==> %s (%s; %s)', theSynset.lemma, theSynset.synsetOffset, theSynset.pos, relatedSynset.pointerSymbol, relatedSynset.lemma, relatedSynset.synsetOffset, relatedSynset.pos);
 
-
-                var filteredPointers = relatedSynset.ptrs.filter(function(ptr) {
-                    return ptr.pointerSymbol === "@";
-                });
-                findRelatedSynsets(filteredPointers, callback);
+                findRelatedSynsets(relatedSynset, callback);
             });
         };
         
-        findRelatedSynsets(filteredPointers, callback);
+        findRelatedSynsets(synset, callback);
 
      });
 });
 
 
-function findRelatedSynsets(pointers, cb) {
-    wordnet.loadSynonyms([], [], _.clone(pointers), function(relatedSynsets) {
+function findRelatedSynsets(synset, cb) {
+    var filteredPointers = synset.ptrs;
+    filteredPointers = synset.ptrs.filter(function(ptr) {
+        return ptr.pointerSymbol === "@";
+    });
+
+    wordnet.loadSynonyms([], [], _.clone(filteredPointers), function(relatedSynsets) {
         _.forEach(relatedSynsets, function(relatedSynset) {
 
-            var symbol = _.find(pointers, function(p) {
+            var symbol = _.find(filteredPointers, function(p) {
                 return p.synsetOffset === relatedSynset.synsetOffset;
             }).pointerSymbol;
 
@@ -52,6 +56,6 @@ function findRelatedSynsets(pointers, cb) {
             // console.log('%s (%s; %s) ==[%s]==> %s (%s; %s)', synset.lemma, synset.synsetOffset, synset.pos, symbol, relatedSynset.lemma, relatedSynset.synsetOffset, relatedSynset.pos);
         });
 
-        cb(relatedSynsets);
+        cb(synset, relatedSynsets);
     });
 }
