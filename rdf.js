276d66918b10c831fe7ed811e21337d9b07d1ddc
pawelrychlik <pawel.rychlik@rspective.pl>
2015-08-11 22:01:26 +0200

    processing neo4j ops in batch

--- rdf.js
+++ rdf.js
@@ -4,8 +4,8 @@ var N3 = require('n3');
 
 
 var db = require("seraph")("http://localhost:7474");
-db.index.createIfNone('Synset', 'wnid', console.log);
-db.constraints.uniqueness.createIfNone('Synset', 'wnid', console.log);
+db.index.createIfNone('Synset', 'wnid', function(err, res) { err && console.log(err); });
+db.constraints.uniqueness.createIfNone('Synset', 'wnid', function(err, res) { err && console.log(err); });
 
 function testSaveNode() {
     db.save({
@@ -19,17 +19,20 @@ function testSaveNode() {
 // return testSaveNode();
 
 // var rdfStream = fs.createReadStream('./../wordnet-3.0-rdf/rdf/rdf/basic/void.ttl');
-// download the n-triple data form http://wordnet-rdf.princeton.edu direct link: http://wordnet-rdf.princeton.edu/wn31.nt.gz
+// download the n-triple data from http://wordnet-rdf.princeton.edu direct link: http://wordnet-rdf.princeton.edu/wn31.nt.gz
 var rdfStream = fs.createReadStream('./../data/wordnet.nt');
 var streamParser = N3.StreamParser();
 rdfStream.pipe(streamParser);
 streamParser.pipe(new ConsumerStream());
 
 
-var predicates = {};
+var predicates = {}; // a map - just to see what predicates are there in the dataset
 var counter = 0;
+var txCounter = 0;
 
 function ConsumerStream() {
+
+    var tx = db.batch();
 
     var writer = new require('stream').Writable({ objectMode: true });
     writer._write = function (triple, encoding, done) {
@@ -55,37 +58,55 @@ function ConsumerStream() {
         predicates[pred] = triple.object;
 
         // console.log('%s (%s) ==[%s]==> %s', wnid, pos, pred, triple.object)
-        // console.log(triple);
-        // setTimeout(done, 100);
 
         if (pred !== 'label') {
             done();
             return;
         }
 
-        var obj = {
+        tx.save({
             label: 'Synset',
             wnid: wnid,
             pos: pos,
             value: value,
             lang: lang
-        };
-        db.save(obj, function(err, result) {
-            console.log(err || result);
-            done();
         });
+
+        if (++txCounter % 5000 === 0) {
+            tx.commit(function(err, results) {
+                console.log(err || "COMMITTED, total: %s", txCounter);
+
+                tx = db.batch();
+
+                done();
+            });
+        } else {
+            done();
+        }
     }
+
+    writer.on('finish', function() {
+        console.log('stream finished')
+        tx.commit(function(err, results) {
+            console.log(err || "COMMITTED, total: %s", txCounter);
+        });
+    });
+    writer.on('error', console.error);
+
     return writer;
 }
 
-process.on('SIGINT', function() {
-    console.log("Counter: %s", counter);
+function printStats() {
+    console.log("N-triples processed: %s", counter);
+    console.log("Created synsets: %s", txCounter);
     console.log(predicates);
     console.log(_.keys(predicates));
-
+}
+process.on('exit', printStats);
+process.on('SIGINT', function() {
+    printStats();
     process.exit();
 });
-
 
 // idea: run the whole thing twice:
 // 1. create only nodes (labels) in batches
